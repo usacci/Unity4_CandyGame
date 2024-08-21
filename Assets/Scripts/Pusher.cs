a4932bb3864057fd2ab94984d915decf64697ffd
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-08-21 09:22:26 +0900

    0821 09:00初期状態

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    public float amplitude;
+    public float speed;
+
+    void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        // 変位を計算
+        float z = amplitude * Mathf.Sin(Time.time * speed);
+
+        // zを変位させたポジションに再設定
+        transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
